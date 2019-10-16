5feec9472015e1c71af68e431f052eb01b6b8904
Steve <stg7@gmx.de>
2019-10-16 14:53:44 +0200

    initital commit of avc parser for qp and framestats

---
+++ README.md
@@ -0,0 +1,45 @@
+# hevc_bitstreamparser
+extracts qp values from avc bitstreams
+
+this project is based on https://ffmpeg.org/releases/ffmpeg-4.2.tar.xz
+modifications to ffmpeg are mostly done in h264dec.c and are marked.
+The ffmpeg-4.2 folder contains separate licences for FFmpeg.
+
+Author: Steve Göring
+
+## requirements
+for ubuntu 18.04 just run `./prepare.sh`, all requirements will be installed
+
+otherwise install:
+```
+autoconf automake build-essential cmake git \
+        libass-dev libfreetype6-dev libsdl2-dev libtheora-dev libtool \
+        libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+        libxcb-xfixes0-dev mercurial pkg-config texinfo wget \
+        zlib1g-dev yasm
+```
+and uncomment `requirements` in `prepare.sh` and run `./prepare.sh`
+
+## Usage
+Assuming that the prepare script successfully compiles the patched ffmpeg version it is possible to extract bitstream statistics with
+```
+./hevc_parser.py
+
+
+usage: avc_parser.py [-h] [--report_file REPORT_FILE] [--nostdout] video_file
+
+extract qp values from avc (h.264) encoded videos
+
+positional arguments:
+  video_file            input video file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --report_file REPORT_FILE
+                        file to store the report, supports compression, e.g.
+                        using report.bz2 (default: report.json.bz2)
+  --nostdout            no report output on stdout (default: False)
+
+stg7 2019
+
+```
---
+++ prepare.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# This file is part of hevc_parser.
+# hevc_parser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# hevc_parser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with hevc_parser. If not, see <http://www.gnu.org/licenses/>.
+
+requirements() {
+    sudo apt-get update -qq
+
+    sudo apt-get -y install autoconf automake build-essential cmake git \
+        libass-dev libfreetype6-dev libsdl2-dev libtheora-dev libtool \
+        libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+        libxcb-xfixes0-dev mercurial pkg-config texinfo wget \
+        zlib1g-dev yasm
+        # exclude those dependencies to remove external decoding parts
+        #libx264-dev libx265-dev libvpx-dev
+}
+
+requirements
+
+cd ffmpeg-4.2
+
+echo "configure ffmpeg"
+./configure --pkg-config-flags="--static" \
+  --disable-doc \
+  --enable-pthreads \
+  --enable-debug=2 \
+  --disable-nvenc \
+  --disable-libx264 \
+  --enable-decoder=h264 \
+  --enable-parser=h264 \
+  --enable-demuxer=h264 \
+  --disable-vaapi
+
+make -j 12
